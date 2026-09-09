f66394a0fb32f2b8c66a2c8020eba76851c9d17a
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 06:18:24 +0800

    更新 twrp_cannon.mk

--- twrp_cannon.mk
+++ twrp_cannon.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pbrp stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+#$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from cannon device
 $(call inherit-product, device/xiaomi/cannon/device.mk)
