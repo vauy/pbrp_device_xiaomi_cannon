567d30117433b9f41632526f1641221a55a6aa75
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 05:42:20 +0800

    更新 BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,3 +107,9 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# OrangeFox Configuration
+FOX_VERSION := R12.1
+FOX_BUILD_TYPE := Unofficial
+FOX_ENABLE_KERNELSU_SUPPORT := true
+
