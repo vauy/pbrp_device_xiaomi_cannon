c81b00cf8a746d6c46fb1857fd6443bb198dba18
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 06:13:07 +0800

    更新 vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo pb_cannon-user
-add_lunch_combo pb_cannon-userdebug
-add_lunch_combo pb_cannon-eng
+add_lunch_combo twrp_cannon-user
+add_lunch_combo twrp_cannon-userdebug
+add_lunch_combo twrp_cannon-eng
