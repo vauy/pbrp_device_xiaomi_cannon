2486ca8fea35779805e52d98e9fb3fba5cfea707
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 07:13:14 +0800

    更新 BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,10 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 2048      
+# 默认亮度（一般取最大值的一半）
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # OrangeFox Configuration
 FOX_MAINTAINER_PATCH_VERSION := 121
