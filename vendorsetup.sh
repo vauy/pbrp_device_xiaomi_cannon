05b96b88430f7f61079d274d0f3ccd6b0e2315f6
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 23:36:18 +0800

    更新 vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,10 @@
-#
+#!/system/bin/sh
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
-#!/system/bin/sh
-#!/system/bin/sh
+
+# SPDX-License-Identifier: Apache-2.0
+
+
 export TW_DEFAULT_LANGUAGE="zh"
 export COMMON_LUNCH_CHOICES="twrp_cannon-eng twrp_cannon-userdebug"
