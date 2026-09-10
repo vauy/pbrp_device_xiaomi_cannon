33627e85125c5a7d6e337f4b539a0c52b4e5b4bb
vauy <44526469+vauy@users.noreply.github.com>
2026-09-10 23:25:01 +0800

    更新 BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,8 +107,8 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_MAX_BRIGHTNESS := 2048
-TW_DEFAULT_BRIGHTNESS := 2048      
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 2047      
 # 默认亮度（一般取最大值的一半）
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 
